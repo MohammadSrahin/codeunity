1cdf90ffa81b141bb8c7e7e44f3b345207cd7c92
Debo15 <40213379+Debo15@users.noreply.github.com>
2023-02-01 00:39:26 +0300

    add backend

---
+++ backend/src/main/java/com/example/codeunity/CodeunityApplication.java
@@ -0,0 +1,13 @@
+package com.example.codeunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeunityApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/example/codeunity/CodeunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codeunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
